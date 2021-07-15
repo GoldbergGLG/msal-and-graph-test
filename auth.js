bee2d1c7394fcb3f6d0e19326cecbce9e1a5d6b9
David L. Goldberg <dgoldberg@glgroup.com>
2021-07-15 15:27:23 -0400

    :shower: Cleanup

--- auth.js
+++ auth.js
@@ -4,8 +4,7 @@ async function getToken() {
   // initialize "Confidential" Client Application (as opposed to "Public" ie. browser applications)
   const tokenResponse = await new ConfidentialClientApplication({
     auth: {
-      authority:
-        `https://login.microsoftonline.com/${process.env.TENANT_ID}`,
+      authority: `https://login.microsoftonline.com/${process.env.TENANT_ID}`,
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET, // Only for Confidential Client Applications
     },
@@ -24,4 +23,4 @@ async function getToken() {
   return tokenResponse.accessToken;
 }
 
-module.exports = { getToken }
+module.exports = { getToken };
